72aec9140803f05c4d6a4a43ad1a8c8ba5f76d03
João Araujo <jjvictorac@gmail.com>
2022-02-15 22:47:41 -0300

    quizzes filter

--- js/script.js
+++ js/script.js
@@ -16,12 +16,26 @@ function pickUpAllQuizzes() {
 
 function showAllQuizzes() {
     const allQuizes = document.querySelector(".all-quizzes");
-    quizzes.forEach(imgQuizz => {
-        if(imgQuizz.title)
-        allQuizes.innerHTML += `<li ><img class="home-quiz-image" src="${imgQuizz.image}" alt="imagem quiz"></li>`;
-    });
+    for (let i = 0; i < quizzes.length; i++) {
+        if (i === 0) {
+            allQuizes.innerHTML += `<li><img class="home-quiz-image" src="${quizzes[i].image}" alt="imagem quiz"></li>`;
+        }
+        else {
+            if (quizzes[i].title !== quizzes[i - 1].title) {
+                allQuizes.innerHTML += `<li><img class="home-quiz-image" src="${quizzes[i].image}" alt="imagem quiz"></li>`;
+            }
+        }
+    }
+    // quizzes.forEach(imgQuizz => {
+    //     if(imgQuizz.title)
+    //     allQuizes.innerHTML += `<li ><img class="home-quiz-image" src="${imgQuizz.image}" alt="imagem quiz"></li>`;
+    // });
 }
 
+function createQuizz() {
+    const main = document.querySelector("main");
+    main.innerHTML = "";
+}
 
 
 pickUpAllQuizzes();
