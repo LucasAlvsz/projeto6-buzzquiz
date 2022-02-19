ebe59ca60d5d04fd225deac906a39d1090e2ca5f
Lucas Alves Rodrigues <contato.lucasalv@gmail.com>
2022-02-19 18:28:09 -0300

    commit backup

--- js/script.js
+++ js/script.js
@@ -1,19 +1,48 @@
-
-let quizzes = [];
-const URL = "https://mock-api.driven.com.br/api/v4/buzzquizz/";
-
+let quizzes = []
+// const URL = "https://mock-api.driven.com.br/api/v4/buzzquizz/"
+let existsUserQuizzes = false
 
 // Pega os quizzes do servidor
 function pickUpAllQuizzes() {
-    axios.get(`${URL}quizzes`).then(showAllQuizzes).catch((erro) => console.log(`${erro.response.status} Erro ao pegar os quizzes`));
+    axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes").then(aleatorio).catch((erro) => console.log(`${erro.response.status} Erro ao pegar os quizzes`));
 }
-
-// Mostra os quizzes na tela inicial
-function showAllQuizzes(quizzesArray) {
-    quizzes = quizzesArray.data
-    const allQuizes = document.querySelector(".all-quizzes");
+function aleatorio(teste){
+    quizzes = teste.data
+    console.log(quizzes);
+    pickUpUserQuizzes()
+}
+function pickUpUserQuizzes() {
+    const userQuizzesList = document.querySelector(".user-quizzes-list")
     for (let i = 0; i < quizzes.length; i++) {
-        allQuizes.innerHTML += `<li onclick="showPageQuizz(quizzes[${i}])"><p class="quizz qzz${i}">${quizzes[i].title}</p>`;
+        if (localStorage.getItem(quizzes[i].id.toString())) {
+            existsUserQuizzes = true
+            let userQuizz = JSON.parse(localStorage.getItem(quizzes[i].id))
+            userQuizzesList.innerHTML +=
+                `<li onclick="showPageQuizz(${quizzes[i].id})"><p class="quizz user-qzz${i}">${userQuizz.title}</p>`
+            document.querySelector(`.user-qzz${i}`).style.background = `linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 64.58%, #000000 100%), url(${userQuizz.image})`
+        }
+    }
+    if (existsUserQuizzes) {
+        console.log("entrei");
+        const noQuizCreatedClass = document.querySelector(".no-quiz-created")
+        noQuizCreatedClass.classList.add("hidden")
+        document.querySelector(".user-quizzes span").innerHTML +=
+            `
+            <div class="title">
+                <p>Seus Quizzes</p>
+                <ion-icon name="add-circle-sharp"></ion-icon>
+            </div>
+            `
+    }
+    showAllQuizzes(quizzes)
+
+}
+// Mostra os quizzes na tela inicial
+function showAllQuizzes(quizzes) {
+    const allQuizzesList = document.querySelector(".all-quizzes-list");
+    for (let i = 0; i < quizzes.length; i++) {
+        allQuizzesList.innerHTML +=
+            `<li onclick="showPageQuizz(quizzes[${i}])"><p class="quizz qzz${i}">${quizzes[i].title}</p>`
         document.querySelector(`.qzz${i}`).style.background = `linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(0, 0, 0, 0.5) 64.58%, #000000 100%), url(${quizzes[i].image})`
     }
 }
---
+++ js/user-quizzes.js
@@ -0,0 +1,81 @@
+const myQuizz = {
+	title: "Título do quizz",
+	image: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Akali_0.jpg",
+	questions: [
+		{
+			title: "Título da pergunta 1",
+			color: "#123456",
+			answers: [
+				{
+					text: "Texto da resposta 1",
+					image: "https://http.cat/411.jpg",
+					isCorrectAnswer: true
+				},
+				{
+					text: "Texto da resposta 2",
+					image: "https://http.cat/412.jpg",
+					isCorrectAnswer: false
+				}
+			]
+		},
+		{
+			title: "Título da pergunta 2",
+			color: "#123456",
+			answers: [
+				{
+					text: "Texto da resposta 1",
+					image: "https://http.cat/411.jpg",
+					isCorrectAnswer: true
+				},
+				{
+					text: "Texto da resposta 2",
+					image: "https://http.cat/412.jpg",
+					isCorrectAnswer: false
+				}
+			]
+		},
+		{
+			title: "Título da pergunta 3",
+			color: "#123456",
+			answers: [
+				{
+					text: "Texto da resposta 1",
+					image: "https://http.cat/411.jpg",
+					isCorrectAnswer: true
+				},
+				{
+					text: "Texto da resposta 2",
+					image: "https://http.cat/412.jpg",
+					isCorrectAnswer: false
+				}
+			]
+		}
+	],
+	levels: [
+		{
+			title: "Título do nível 1",
+			image: "https://http.cat/411.jpg",
+			text: "Descrição do nível 1",
+			minValue: 0
+		},
+		{
+			title: "Título do nível 2",
+			image: "https://http.cat/412.jpg",
+			text: "Descrição do nível 2",
+			minValue: 50
+		}
+	]
+}
+
+//axios.post("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes", myQuizz).then(deubom)
+
+function deubom(quizzesArray) {
+	console.log("dale");
+	saveUserQuizz(quizzesArray.data.id, myQuizz)
+}
+
+function saveUserQuizz(id, myQuizz) {
+	userQuizz = JSON.stringify(myQuizz)
+	localStorage.setItem(id.toString(), userQuizz)
+}
+//localStorage.setItem("nome", "Lucax");
